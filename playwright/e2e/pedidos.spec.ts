06b2a84fce05a7bce85cdf3e76066b34b053670d
Sidinei <sydney.ramos@hotmail.com>
2026-01-26 11:19:43 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,19 +1,19 @@
 import { test, expect } from '@playwright/test'
 
+/// AAA - Arrange, Act, Assert
+
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  // Arrange
   await page.goto('http://localhost:5173/')
-
-  // Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-  // Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
-  
-  await page.getByTestId('search-order-id').fill('VLO-KU5BCG')
 
+  // Act
+  await page.getByTestId('search-order-id').fill('VLO-KU5BCG')
   await page.getByTestId('search-order-button').click()
 
+  // Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-KU5BCG')
 
