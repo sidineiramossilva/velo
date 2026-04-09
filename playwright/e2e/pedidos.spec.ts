e7285e9a1f1a344b4876844e84429864a8c5623d
Sidinei <sydney.ramos@hotmail.com>
2026-04-09 14:31:41 -0300

    atualizando o projeto para ficar igual da aula

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,25 +1,19 @@
 import { expect, test } from '../support/fixtures'
 import type { OrderDetails } from '../support/actions/orderLockupActions'
 import { generateOrderCode } from '../support/helpers'
+import { insertOrder, deleteOrderByNumber } from '../support/database/orderRepository'
+import testData from '../support/fixtures/orders.json' with { type: 'json' }
 
 test.describe('Consulta de Pedido', () => {
-
   test.beforeEach(async ({ app }) => {
     await app.orderLockup.open()
   })
 
   test('deve consultar um pedido aprovado', async ({ app }) => {
-    const order: OrderDetails = {
-      number: 'VLO-KU5BCG',
-      status: 'APROVADO' as const,
-      color: 'Lunar White',
-      wheels: 'aero Wheels',
-      customer: {
-        name: 'Sidinei Silva',
-        email: 'silva@dev.com'
-      },
-      payment: 'À Vista'
-    }
+    const order: OrderDetails = testData.aprovado as OrderDetails
+
+    await deleteOrderByNumber(order.number)
+    await insertOrder(order)
 
     await app.orderLockup.searchOrder(order.number)
     await app.orderLockup.validateOrderDetails(order)
@@ -27,17 +21,10 @@ test.describe('Consulta de Pedido', () => {
   })
 
   test('deve consultar um pedido reprovado', async ({ app }) => {
-    const order: OrderDetails = {
-      number: 'VLO-QQ0XKJ',
-      status: 'REPROVADO' as const,
-      color: 'Midnight Black',
-      wheels: 'sport Wheels',
-      customer: {
-        name: 'Steve Jobs',
-        email: 'jobs@apple.com'
-      },
-      payment: 'À Vista'
-    }
+    const order: OrderDetails = testData.reprovado as OrderDetails
+
+    await deleteOrderByNumber(order.number)
+    await insertOrder(order)
 
     await app.orderLockup.searchOrder(order.number)
     await app.orderLockup.validateOrderDetails(order)
@@ -45,17 +32,10 @@ test.describe('Consulta de Pedido', () => {
   })
 
   test('deve consultar um pedido em análise', async ({ app }) => {
-    const order: OrderDetails = {
-      number: 'VLO-ZUXI75',
-      status: 'EM_ANALISE' as const,
-      color: 'Lunar White',
-      wheels: 'aero Wheels',
-      customer: {
-        name: 'João da Silva',
-        email: 'joao@velo.dev'
-      },
-      payment: 'À Vista'
-    }
+    const order: OrderDetails = testData.em_analise as OrderDetails
+
+    await deleteOrderByNumber(order.number)
+    await insertOrder(order)
 
     await app.orderLockup.searchOrder(order.number)
     await app.orderLockup.validateOrderDetails(order)
@@ -63,15 +43,13 @@ test.describe('Consulta de Pedido', () => {
   })
 
   test('deve exibir mensagem quando o pedido não é encontrado', async ({ app }) => {
-
     const order = generateOrderCode()
 
     await app.orderLockup.searchOrder(order)
-    await app.orderLockup.validateOrderNotFound()    
+    await app.orderLockup.validateOrderNotFound()
   })
 
   test('deve exibir mensagem quando o código do pedido está fora do padrão', async ({ app }) => {
-
     const orderCode = 'XYZ-999-INVALIDO'
 
     await app.orderLockup.searchOrder(orderCode)
